0318500615d0325d46006253ac88c3b37516382e
ykliba <mediaboxing2@gmail.com>
2020-04-25 12:53:49 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :items, only: [:index, :new, :create]
 end
