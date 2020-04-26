70a667e929a1399e78818cd7747275bca240fa80
AiliS-pro <chopper.man.015@gmail.com>
2020-04-26 18:08:44 +0900

    商品画像のマイグレーションファイル作成

--- db/migrate/20200426090220_create_item_photos.rb
+++ db/migrate/20200426090220_create_item_photos.rb
@@ -1,7 +1,8 @@
 class CreateItemPhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :item_photos do |t|
-
+      t.references :item, foreign_key: true
+      t.string :image, null: false
       t.timestamps
     end
   end
