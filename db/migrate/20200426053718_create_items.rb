3511af146018d2103e3f84da07037ceedd00ca1e
AiliS-pro <chopper.man.015@gmail.com>
2020-04-26 17:07:12 +0900

    商品のマイグレーションファイル作成

--- db/migrate/20200426053718_create_items.rb
+++ db/migrate/20200426053718_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.references :user, foreign_key: true
+      t.references :brand, foreign_key: true
+      t.string :name, null: false
+      t.text :explain, null: false
+      t.string :status, null: false
+      t.string :shipping_fee, null: false
+      t.string :shipping_area, null: false
+      t.string :shipping_days, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
