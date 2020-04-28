240974982d3cd12823591760f5e79e7da994fbf0
AiliS-pro <chopper.man.015@gmail.com>
2020-04-28 19:19:50 +0900

    商品一覧機能(サーバサイド)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   belongs_to :brand
   has_many :item_photos, dependent: :destroy
   has_many :categories, dependent: :destroy
+  mount_uploader :image, ImageUploader
+  def thumbnail
+    return self.image.variant(resize: '300x300').processed
+  end
 end
