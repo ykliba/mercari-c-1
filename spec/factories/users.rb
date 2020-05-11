b51e4e9fecd2840fa9c254801d2cd9c283e32ac4
motuto <ryuuma0425@gmail.com>
2020-05-11 16:25:59 +0900

    email Validation correction and test execution

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :user do
     nickname               {"abe"}
-    email                  {"kkk@gmail.com"}
+    email                  {"aa@aa.com"}
     password               {"0000000"}
     password_confirmation  {"0000000"}
     
