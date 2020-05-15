8a80e46581473a6d3b37948ff4cae9d773c88dc4
motuto <ryuuma0425@gmail.com>
2020-05-15 22:36:49 +0900

    ユーザーテストとバリデーション の実装

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -3,4 +3,5 @@ class DeliveryAddress < ApplicationRecord
   validates :delivery_firstname, :delivery_lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :delivery_firstname_hiragana, :delivery_lastname_hiragana, format: { with: /\A[ぁ-んー－]+\z/ }
   validates :postal_code, length: { is: 7 } 
+  validates :prefecture, :city, :address, presence: true
 end
